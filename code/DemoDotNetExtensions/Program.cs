30667fa6cf429306e305c301dbc3295e5c5d4ac3
Jorge Serrano <jorge.serrano.perez@gmail.com>
2017-01-29 22:44:20 +0100

    Uploaded more extensions to the code.

--- code/DemoDotNetExtensions/Program.cs
+++ code/DemoDotNetExtensions/Program.cs
@@ -9,26 +9,53 @@
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Demo - DotNetExtensions");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            var title = "Demo - DotNetExtensions";
+            Console.WriteLine('-'.Repeat(title.Length));
+            Console.WriteLine(title);
 
-            // StringExtensions - Repeat
+
+            // ************************************
+            // StringExtensions
+            // ************************************
+            // Repeat
             Console.WriteLine('-'.Repeat(23));
             Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("StringExtensions");
+            Console.ForegroundColor = ConsoleColor.White;
+            var text = "Sample.here";
+            char[] characters = { ',', 'H' };
+            // ContainsCharacters
+            Console.WriteLine("ContainsCharacters " + text.ContainsCharacters(characters, StringComparison.OrdinalIgnoreCase));
+            // ContainsText
+            Console.WriteLine("ContainsText " + text.ContainsText("Her", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine();
+            // ************************************
+
+
+
+            // ************************************
             // ExceptionExtensions
+            // ************************************
             try
             {
                 DemoExceptionExtensions.GenerateException();
             }
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("ExceptionExtensions");
+                Console.ForegroundColor = ConsoleColor.White;
                 var info = ex.GetMessagesFromInnerExceptions().ToString();
                 Console.WriteLine(info);
                 Console.WriteLine();
             }
+            // ************************************
 
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Press any key to close the App");
             Console.ReadLine();
         }
--- code/DotNetExtensions/StringExtensions.cs
+++ code/DotNetExtensions/StringExtensions.cs
@@ -6,6 +6,38 @@
     public static class StringExtensions
     {
 
+        public static bool ContainsCharacters(this string source, char[] characters, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            if (characters.Length == 0 || 
+                String.IsNullOrEmpty(source))
+            {
+                return true;
+            }
+
+            foreach (var character in characters)
+            {
+                var text = String.Join(String.Empty, character);
+                if (source.ContainsText(text, stringComparison))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ContainsText(this string source, string pattern, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            if (String.IsNullOrEmpty(pattern) ||
+                String.IsNullOrEmpty(source) ||
+                source.IndexOf(pattern, stringComparison) >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         public static string Repeat(this char c, int count)
         {
             return new String(c, count);
